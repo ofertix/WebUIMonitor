ef397667d9f0099a213a20667e39e5f67dad5110
Jordi Llonch <jordi@laigu.net>
2012-06-01 12:29:35 +0200

    websocket library replaced with https://github.com/lemmingzshadow/php-websocket

--- vendor/php-websocket/server/lib/WebSocket/Application/StatusApplication.php
+++ vendor/php-websocket/server/lib/WebSocket/Application/StatusApplication.php
@@ -1,51 +1,118 @@
 <?php
+namespace WebSocket\Application;
+
 /**
- * Displays server-status. Connected clients, e.g.
+ * Shiny WSS Status Application
+ * Provides live server infos/messages to client/browser.
  * 
  * @author Simon Samtleben <web@lemmingzshadow.net>
  */
-
-namespace WebSocket\Application;
-
 class StatusApplication extends Application
 {
-    private $clients = array();
+    private $_clients = array();
+	private $_serverClients = array();
+	private $_serverInfo = array();
+	private $_serverClientCount = 0;
 
-    public function onConnect($client)
+
+	public function onConnect($client)
     {
-        $this->clients[] = $client;		
+		$id = $client->getClientId();
+        $this->_clients[$id] = $client;
+		$this->_sendServerinfo($client);
     }
 
     public function onDisconnect($client)
     {
-        $key = array_search($client, $this->clients);
-        if($key)
-		{
-            unset($this->clients[$key]);
-        }
+        $id = $client->getClientId();		
+		unset($this->_clients[$id]);     
     }
 
-	public function onData($data, $client)
-    {
-		// currently client can not send any data.
-        return false;
+    public function onData($data, $client)
+    {		
+        // currently not in use...
     }
 	
-	public function clientConnected($id, $ip, $port)
-	{		
-		$this->_sendAll('Client connected: ' . $id . ' (' . $ip . ':' . $port . ')');
-	}
-	
-	public function clientDisconnected($id, $ip, $port)
+	public function setServerInfo($serverInfo)
 	{
-		$this->_sendAll('Client disconnected: ' . $id . ' (' . $ip . ':' . $port . ')');
-	}
-	
-	private function _sendAll($data)
-	{
-		foreach($this->clients as $sendto)
+		if(is_array($serverInfo))
 		{
-            $sendto->send($data);
+			$this->_serverInfo = $serverInfo;
+			return true;
+		}
+		return false;
+	}
+
+
+	public function clientConnected($ip, $port)
+	{
+		$this->_serverClients[$port] = $ip;
+		$this->_serverClientCount++;
+		$this->statusMsg('Client connected: ' .$ip.':'.$port);
+		$data = array(
+			'ip' => $ip,
+			'port' => $port,
+			'clientCount' => $this->_serverClientCount,
+		);
+		$encodedData = $this->_encodeData('clientConnected', $data);
+		$this->_sendAll($encodedData);
+	}
+	
+	public function clientDisconnected($ip, $port)
+	{
+		if(!isset($this->_serverClients[$port]))
+		{
+			return false;
+		}
+		unset($this->_serverClients[$port]);
+		$this->_serverClientCount--;
+		$this->statusMsg('Client disconnected: ' .$ip.':'.$port);
+		$data = array(			
+			'port' => $port,
+			'clientCount' => $this->_serverClientCount,
+		);
+		$encodedData = $this->_encodeData('clientDisconnected', $data);
+		$this->_sendAll($encodedData);
+	}
+	
+	public function clientActivity($port)
+	{
+		$encodedData = $this->_encodeData('clientActivity', $port);
+		$this->_sendAll($encodedData);
+	}
+
+	public function statusMsg($text, $type = 'info')
+	{		
+		$data = array(
+			'type' => $type,
+			'text' => '['. strftime('%m-%d %H:%M', time()) . '] ' . $text,
+		);
+		$encodedData = $this->_encodeData('statusMsg', $data);		
+		$this->_sendAll($encodedData);
+	}
+	
+	private function _sendServerinfo($client)
+	{
+		if(count($this->_clients) < 1)
+		{
+			return false;
+		}
+		$currentServerInfo = $this->_serverInfo;
+		$currentServerInfo['clientCount'] = count($this->_serverClients);
+		$currentServerInfo['clients'] = $this->_serverClients;
+		$encodedData = $this->_encodeData('serverInfo', $currentServerInfo);
+		$client->send($encodedData);
+	}
+	
+	private function _sendAll($encodedData)
+	{		
+		if(count($this->_clients) < 1)
+		{
+			return false;
+		}
+		foreach($this->_clients as $sendto)
+		{
+            $sendto->send($encodedData);
         }
 	}
 }
